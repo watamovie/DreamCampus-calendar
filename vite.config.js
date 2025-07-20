824707ebccdcf9fd5b0eea30f35ad35c7ad4bb84
Wata_movie <85270829+watamovie@users.noreply.github.com>
2025-07-20 13:00:19 +0900

    build how-to page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2020'
+    target: 'es2020',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        howto: resolve(__dirname, 'howto.html')
+      }
+    }
   },
   server: {
     open: true,
